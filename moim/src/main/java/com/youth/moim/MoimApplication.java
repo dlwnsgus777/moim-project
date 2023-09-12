2540d36672a1a75dbcbf0b0766e2fca00523071f
Ted <dlwnsgus777@naver.com>
2023-09-12 13:41:03 +0900

    3.add dependencies

--- moim/src/main/java/com/youth/moim/MoimApplication.java
+++ moim/src/main/java/com/youth/moim/MoimApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MoimApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MoimApplication.class, args);
 	}
