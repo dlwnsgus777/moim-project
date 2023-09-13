b35d98cfde7b40dd2d31d9c699341fa92dcd8440
Ted <dlwnsgus777@naver.com>
2023-09-13 15:49:01 +0900

    5. create sign in test
    5. create sign in test

--- moim/src/test/java/com/youth/moim/presentation/user/UserApiTest.java
+++ moim/src/test/java/com/youth/moim/presentation/user/UserApiTest.java
@@ -1,5 +1,6 @@
 package com.youth.moim.presentation.user;
 
+import groovy.transform.builder.Builder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,15 +9,21 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserApiTest {
   private UserController userController;
   private UserRepository userRepository;
+
   @BeforeEach
   void setUp() {
-
+    userRepository = new UserRepository();
+    UserStore userStore = new UserStore(userRepository);
+    UserReader userReader = new UserReader(userRepository);
+    UserService userService = new UserService(userStore, userReader);
+    userController = new UserController(userService);
   }
 
   @Test
@@ -32,13 +39,14 @@ public class UserApiTest {
             "dlwnsgus777@test.com",
             "company"
     );
-    System.out.println(request.gender());
 
     // when
     userController.signIn(request);
 
     // then
+    User user = userRepository.findByEmail(request.email()).orElseThrow(() -> new IllegalArgumentException("저장실패"));
     assertThat(userRepository.findAll().size()).isNotEqualTo(0);
+    assertThat(user.getEmail()).isEqualTo(request.email());
 
   }
 
@@ -53,6 +61,18 @@ public class UserApiTest {
             String company
     ) {
 
+      public User toEntity() {
+        return new User(
+                null,
+                name,
+                birth,
+                gender,
+                id,
+                password,
+                email,
+                company
+        );
+      }
     }
   }
 
@@ -70,7 +90,10 @@ public class UserApiTest {
     }
 
     public void signIn(UserRequest.SignInOrganizer request) {
-
+      Optional<User> user = userReader.findByEmail(request.email());
+      if (user.isEmpty()) {
+        userStore.registerUser(request.toEntity());
+      }
     }
   }
 
@@ -87,9 +110,27 @@ public class UserApiTest {
   }
 
   public static class UserStore {
+    private final UserRepository userRepository;
+
+    public UserStore(UserRepository userRepository) {
+      this.userRepository = userRepository;
+    }
+
+    public void registerUser(User entity) {
+      userRepository.save(entity);
+    }
   }
 
   public static class UserReader {
+    private final UserRepository userRepository;
+
+    public UserReader(UserRepository userRepository) {
+      this.userRepository = userRepository;
+    }
+
+    public Optional<User> findByEmail(String email) {
+      return userRepository.findByEmail(email);
+    }
   }
 
   public static class UserRepository {
@@ -105,6 +146,12 @@ public class UserApiTest {
     public List<User> findAll() {
       return new ArrayList<>(users.values());
     }
+
+    public Optional<User> findByEmail(String email) {
+      return users.values().stream()
+              .filter(user -> user.getEmail().equals(email))
+              .findAny();
+    }
   }
 
   public static class User {
@@ -117,6 +164,7 @@ public class UserApiTest {
     private String email;
     private String company;
 
+    @Builder
     public User(Long idx, String name, String birth, Gender gender, String id, String password, String email, String company) {
       this.idx = idx;
       this.name = name;
@@ -134,6 +182,10 @@ public class UserApiTest {
 
     public void assignId(Long idx) {
       this.idx = idx;
+    }
+
+    public String getEmail() {
+      return email;
     }
   }
 }
