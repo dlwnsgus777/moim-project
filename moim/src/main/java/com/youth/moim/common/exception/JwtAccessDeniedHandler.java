9861dcfa9170c8b6cec7be18c871efc0dc9791dc
Ted <dlwnsgus777@naver.com>
2023-09-18 15:11:42 +0900

    6. config security filter and exception
    6. config security filter and exception

---
+++ moim/src/main/java/com/youth/moim/common/exception/JwtAccessDeniedHandler.java
@@ -0,0 +1,17 @@
+package com.youth.moim.common.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
+        throw new AuthorizationException("인증에 실패하였습니다.");
+    }
+}
--- moim/src/main/java/com/youth/moim/config/SecurityConfiguration.java
+++ moim/src/main/java/com/youth/moim/config/SecurityConfiguration.java
@@ -1,5 +1,10 @@
 package com.youth.moim.config;
 
+import com.youth.moim.application.auth.JwtTokenFilter;
+import com.youth.moim.application.auth.JwtTokenProvider;
+import com.youth.moim.common.exception.JwtAccessDeniedHandler;
+import com.youth.moim.common.exception.JwtAuthenticationEntryPoint;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,10 +14,17 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.ExceptionTranslationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+    private final JwtTokenProvider jwtTokenProvider;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
@@ -23,6 +35,14 @@ public class SecurityConfiguration {
             .authorizeHttpRequests(request -> {
               request.anyRequest().permitAll();
             });
+
+        http.addFilterBefore(new JwtTokenFilter(jwtTokenProvider), ExceptionTranslationFilter.class);
+
+        http.exceptionHandling((exceptionHandling) ->
+              exceptionHandling
+                      .accessDeniedHandler(jwtAccessDeniedHandler)
+                      .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+      );
 
     return http.build();
   }
